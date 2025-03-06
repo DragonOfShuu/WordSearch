2bfee37581486f566beeac056766172367f565aa
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2025-03-06 02:47:14 -0700

    Components Recognize Text Selection

--- wordsearch.client/src/app/core/wordsearch-box/word-selection/word-selection.component.ts
+++ wordsearch.client/src/app/core/wordsearch-box/word-selection/word-selection.component.ts
@@ -1,16 +1,23 @@
 import { Component, input, OnInit } from '@angular/core';
 import { randomInt } from '../../../shared/random/random';
+import { Vector2D } from '../../../shared/types/vector.types';
+import { WordType } from '../../../shared/types/word-dictionary.types';
 
 @Component({
   selector: 'shuu-word-selection',
   imports: [],
   templateUrl: './word-selection.component.html',
-  styleUrl: './word-selection.component.sass',
+  styleUrl: './word-selection.component.sass'
 })
 export class WordSelectionComponent {
   r = input(this.randomColorValue());
   g = input(this.randomColorValue());
   b = input(this.randomColorValue());
+
+  position = input.required<Vector2D>()
+  foundCoords = input.required<{[x: string]: WordType[]}>()
+  selected = input.required<null|Vector2D>()
+  hover = input.required<null|Vector2D>()
 
   randomColorValue() {
     return randomInt(50, 240);
