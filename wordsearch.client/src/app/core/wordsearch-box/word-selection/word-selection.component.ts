931dec95e92bb4615126800d8c3e3387d8c075a3
Dragon of Shuu <68718280+DragonOfShuu@users.noreply.github.com>
2025-03-10 01:11:26 -0600

    Words Can Now Be Selected (Not Found Yet)

--- wordsearch.client/src/app/core/wordsearch-box/word-selection/word-selection.component.ts
+++ wordsearch.client/src/app/core/wordsearch-box/word-selection/word-selection.component.ts
@@ -25,4 +25,35 @@ export class WordSelectionComponent {
   randomColorValue() {
     return randomInt(50, 240);
   }
+
+  rotationToTransform(rot: Vector2D|null): string {
+    if (rot === null) return `rotate(0)`;
+
+    return `rotate(${this.fromRotation(rot)}deg)`
+  }
+
+  fromRotation(rot: Vector2D): number|null {
+    const stringd = `${rot.x},${rot.y}`
+    const conversion: {[rotation: string]: number} = {
+      '1,0': 0,
+      '1,-1': -45,
+      '0,-1': -90,
+      '-1,-1': -135,
+      '-1,0': -180,
+      '-1,1': -225,
+      '0,1': -270,
+      '1,1': -315
+    }
+    return conversion[stringd] ?? null
+  }
+
+  isDiagonal(rot: Vector2D) {
+    return rot.x !== 0 && rot.y !== 0;
+  }
+
+  determineWidth() {
+    const rotation = this.rotation()
+    const diagonalChange = rotation === null || !this.isDiagonal(rotation) ? 1 : Math.sqrt(2)
+    return 100*((this.length() ?? 1)-1) * diagonalChange;
+  }
 }
--- wordsearch.client/src/app/core/wordsearch-box/wordsearch/wordsearch.component.ts
+++ wordsearch.client/src/app/core/wordsearch-box/wordsearch/wordsearch.component.ts
@@ -38,6 +38,14 @@ export class WordsearchComponent {
 
   selectText = output<WordType>();
 
+  /**
+   * Find the amount of tiles that make
+   * the distance from start to end [INCLUSIVE
+   * TO START AND END]
+   * @param start 
+   * @param end 
+   * @returns 
+   */
   #findLength(start: Vector2D, end: Vector2D): number {
     const xLength = Math.abs(start.x - end.x);
     const yLength = Math.abs(start.y - end.y);
@@ -45,6 +53,13 @@ export class WordsearchComponent {
     return (xLength || yLength) + 1;
   }
 
+  /**
+   * Generate a normalized rotation based on
+   * the start and end.
+   * @param start
+   * @param end 
+   * @returns 
+   */
   #find1DRotation(start: number, end: number) {
     return Math.min(Math.max(end - start, -1), 1);
   }
@@ -56,7 +71,15 @@ export class WordsearchComponent {
     };
   }
 
-  #findWord(start: Vector2D, rotation: Vector2D, length: number): string {
+  /**
+   * Gets a word off the board, being provided the starting location
+   * and the rotation and length to follow to collect the word.
+   * @param start The starting location of where we collected the word
+   * @param rotation The rotation we need to take to make it to the word
+   * @param length The length of the word
+   * @returns The new ord
+   */
+  #discoverWord(start: Vector2D, rotation: Vector2D, length: number): string {
     let word = '';
     for (let i = 0; i < length; i++) {
       const newX = i * rotation.x + start.x;
@@ -67,17 +90,19 @@ export class WordsearchComponent {
     return word;
   }
 
-  tileClicked(x: number, y: number) {
-    const selected = this.selectedTile();
-    if (selected === null) {
-      this.selectedTile.set({ x, y });
-      return;
-    }
+  getAssumedEndPosition(start: Vector2D, rotation: Vector2D, length: number): Vector2D {
+    const adjustedLength = length - 1;
+    return {x: start.x + rotation.x*adjustedLength, y: start.y + rotation.y*adjustedLength}
+  }
 
-    if (selected.x === x && selected.y === y) {
-      this.selectedTile.set(null);
-      return;
-    }
+  searchClicked() {
+    console.log("SEARCH CLICKED")
+    const selected = this.selectedTile();
+    if (selected===null) return;
+    const snappedTo = this.snapToTileData()
+    if (snappedTo===null) return;
+
+    const {x, y} = this.getAssumedEndPosition(selected, snappedTo.rotation, snappedTo.length)
 
     if (
       // True if the selected tile is not horizontal or vertical
@@ -89,19 +114,54 @@ export class WordsearchComponent {
       return;
     }
 
+    if (
+      x < 0 
+      || x > this.searchableText()[0].length
+      || y < 0
+      || y > this.searchableText().length
+    ) {
+      console.log("Assumed coords not in range")
+      return;
+    }
+  
     const start: Vector2D = selected;
     const end: Vector2D = { x, y };
+  
+    this.findWord(start, end)
+  }
+  
+  tileClicked(e: MouseEvent, x: number, y: number) {
+    const selected = this.selectedTile();
+    if (selected === null) {
+      this.selectedTile.set({ x, y });
+      e.stopImmediatePropagation();
+      return;
+    }
 
+    if (selected.x === x && selected.y === y) {
+      this.selectedTile.set(null);
+      e.stopImmediatePropagation();
+      return;
+    }
+  }
+  
+  findWord(start: Vector2D, end: Vector2D) {
     const length = this.#findLength(start, end);
-    console.log('Length: ', length);
     const rotation = this.#findRotation(start, end);
-    const word = this.#findWord(start, rotation, length);
-
+    const word = this.#discoverWord(start, rotation, length);
+  
     const wordInfo = { position: start, rotation, word };
     console.log('Selected Word Info: ', wordInfo);
-
+  
     this.selectedTile.set(null);
     this.selectText.emit(wordInfo);
+  }
+
+  rightClickUnselect() {
+    if (this.selectedTile() === null) return true;
+
+    this.selectedTile.set(null);
+    return false; 
   }
 
   unSnap() {
